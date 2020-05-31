3dbde461c2fa6058db7b6bdc829135ffc67e8903
cob <obroinc@gmail.com>
2020-05-31 16:44:42 +0100

    added loading shoppining counter if login again

--- ClubWestRFC/Pages/Members/Home/Index.cshtml.cs
+++ ClubWestRFC/Pages/Members/Home/Index.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using ClubWestRFC.DataAccess.Data.Repository.IRepository;
 using ClubWestRFC.Models;
+using ClubWestRFC.Utility;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -29,7 +32,23 @@ namespace ClubWestRFC.Pages.Members.Home
         //to popluate the lists be able to filter/add properties using repository
         public void OnGet()
         {
-            //
+            //check if user has ckecked in and retrieve the session and shopping cart count
+
+
+
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+           
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim!=null)
+            {
+                int shoppingCartCount = _unitofWork.ShoppingCart.GetAll(a => a.ApplicationUserId == claim.Value).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ShoppingCart, shoppingCartCount);
+             
+            }
+
+
+
             MemberpriceList = _unitofWork.Memberprice.GetAll(null, null, "Category,MembershipType");
 
             CategoryList = _unitofWork.Category.GetAll(null, q => q.OrderBy(c => c.DisplayOrder), null);
